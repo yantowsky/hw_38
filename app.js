33dbf410c4539dd44f6f6e868b3c633a943b72b9
yantowsky <it.yantowsky@gmail.com>
2024-02-27 23:17:02 +0200

    Created new project: Home Work #38 (SIGINT)

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const process = require('node:process');
+
+const arrEvent = [];
+
+process.on("SIGINT", (event) => {
+    arrEvent.push(event);
+    console.log('Just Received SIGINT.');
+    setTimeout(() => {
+        if (arrEvent.length >= 2) {
+            console.log("Stop process NODE.JS!");
+            process.exit(0);
+        } else {
+            arrEvent.length = 0;
+        }
+    }, 3000)
+})
+
+setInterval(() => { }, 2000);
